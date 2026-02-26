36f0ee0d2b3641ca8e95bebddae99428933a8cd1
a8taleb <a8taleb@nixos-hp>
2026-02-26 13:06:16 +0100

    feat: Add node IP outputs

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,18 @@ output "kubeconfig" {
     value       = talos_cluster_kubeconfig.talos_kubeconfig.kubeconfig_raw
     sensitive   = true
 }
+
+output "control_plane_ips" {
+    description = "List of control plane node IPs"
+    value       = local.control_node_ips
+}
+
+output "worker_ips" {
+    description = "List of worker node IPs"
+    value       = local.worker_node_ips
+}
+
+output "all_node_ips" {
+    description = "List of all node IPs (control + workers)"
+    value       = local.node_ips
+}
